f7a3e85bd10a83524440c44ba009cab1b40e96fc
hijin <guu7898@gmail.com>
2025-04-05 02:37:03 +0900

    feat: 메인페이지, 검색페이지, 로그인, 회원가입, 마이페이지 구성요소 배치 완료 - 메인페이지: 배너, 검색창, 게임 리스트 배치 - 검색페이지: 검색 결과 리스트 및 상세 정보 레이아웃 구성 - 로그인 & 회원가입 페이지 레이아웃 정리 - 마이페이지 UI 기본 구조 배치

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import React, { useState } from "react";
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import Navbar from "./components/Navbar";
 import MyPage from "./pages/MyPage";
 import SearchPage from "./pages/SearchPage";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  console.log(Router);
 
   return (
     <Routes>
@@ -17,19 +17,6 @@ function App() {
       <Route path='/signup' element={<Signup />} />
       <Route path="/my" element={<MyPage isLoggedIn={isLoggedIn} />} />
       <Route path="/search/:query" element={<SearchPage isLoggedIn={isLoggedIn}/>} />
-      {/* <Route path="/signup" element={<Signup />} />
-          <Route
-            path="/routine/:id"
-            element={<RoutineDetail setSelectedStore={setSelectedStore} />} // 루틴 상세 페이지
-          />
-          <Route
-            path="/routine/new"
-            element={<RoutineDetail setSelectedStore={setSelectedStore} />} // 루틴 생성 페이지
-          />
-          <Route
-            path="/check"
-            element={<CheckView userId={options.userId} />} // 주문 내역 페이지
-          /> */}
     </Routes>
   );
 }
